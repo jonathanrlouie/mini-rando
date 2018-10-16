88ad49277ff7914e4e022f092e269e5a0a048434
Jonathan Louie <jonathanrlouie@gmail.com>
2018-10-15 23:07:10 -0700

    Updated travis.yml file and trying to get basic amethyst project working

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,40 @@ extern crate rand;
 mod randomizer;
 mod states;
 
+use std::time::Duration;
+
 use amethyst::{
     prelude::*,
+    core::frame_limiter::FrameRateLimitStrategy,
+    renderer::{
+        DrawFlat,
+        PosTex
+    }
 };
 use states::mini_rando::MiniRando;
 
 const FRAME_LIMIT: u32 = 60;
 
-fn main() {
-    //let assets_dir = format!("{}/resources", application_root_dir());
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
 
-    //let mut game = Application::build(assets_dir, MiniRando);
+    let display_config_path = format!(
+        "{}/resources/display.ron",
+        env!("CARGO_MANIFEST_DIR")
+    );
+
+    let assets_dir = format!("{}/resources", env!("CARGO_MANIFEST_DIR"));
+
+    let game_data = GameDataBuilder::default()
+        .with_basic_renderer(display_config_path, DrawFlat::<PosTex>::new(), true)?;
+
+    let mut game = Application::build(assets_dir, MiniRando)?
+        .with_frame_limit(
+            FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2)),
+            FRAME_LIMIT,
+        )
+        .build(game_data)?;
+
+    game.run();
+    Ok(())
 }
--- src/states/mini_rando.rs
+++ src/states/mini_rando.rs
@@ -1,12 +1,22 @@
 use amethyst::{
     prelude::*,
+    renderer::Event,
 };
 
 pub struct MiniRando;
 
-/*
-impl State for MiniRando {
+impl<'a, 'b> State<GameData<'a, 'b>> for MiniRando {
     fn on_start(&mut self, data: StateData<GameData>) {
 
     }
-}*/
+
+    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
+
+        Trans::None
+
+    }
+
+    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        Trans::None
+    }
+}
